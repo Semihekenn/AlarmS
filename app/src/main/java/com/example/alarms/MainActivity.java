943e95940b3a0cfb3eeed7bc4ac575b2edf07e3b
Semihekenn <50514163+Semihekenn@users.noreply.github.com>
2019-11-19 10:51:53 +0300

    asdfaf

--- app/src/main/java/com/example/alarms/MainActivity.java
+++ app/src/main/java/com/example/alarms/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -34,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         activity=this;
         MainAlarm.isopen=true;
